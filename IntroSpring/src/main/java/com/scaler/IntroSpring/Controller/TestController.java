a35351f0c56721b90305e8aafc7523f64c321865
stwarHatLuffy <yadraj1803@gmail.com>
2024-01-08 16:57:33 -0500

    Commit ProductService main

--- IntroSpring/src/main/java/com/scaler/IntroSpring/Controller/TestController.java
+++
@@ -1,2 +0,0 @@
-package com.scaler.IntroSpring.Controller;public class ThisController {
-}
--- ProductService/src/main/java/com/scaler/ProductService/Models/BaseModel.java
+++ ProductService/src/main/java/com/scaler/ProductService/Models/BaseModel.java
@@ -1,2 +1,11 @@
-package com.scaler.ProductService.Models;public class BaseModel {
+package com.scaler.ProductService.Models;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseModel {
+    private Long id;
 }
